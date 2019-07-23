714f1e93a8936cfebd73ac5191155df3c9039fd4
Noah <noah@coronasoftware.net>
2019-07-23 17:10:49 +0000

    Add resource access

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
+#[cfg(test)]
+mod tests;
+
 use tera;
-use actix_web::{web, middleware, error, App, HttpResponse, Error, HttpServer, Responder};
+use std::env;
+use log;
+use std::path::{Path, PathBuf};
+use actix_files as fs;
+use actix_web::{
+    HttpRequest, HttpResponse,
+    Error, HttpServer,
+    web, middleware,
+    error, App,
+    Responder, Result
+};
 
 
 fn index(tmpl: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
@@ -10,17 +23,28 @@ fn index(tmpl: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
 }
 
 
+fn resource(req: HttpRequest) -> Result<fs::NamedFile> {
+    let file: PathBuf = req.match_info().query("filename").parse().unwrap();
+    let path = Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/src/static/images"))
+        .join(file);
+
+    Ok(fs::NamedFile::open(path)?)
+}
+
+
 fn main() {
-    std::env::set_var("RUST_LOG", "actix_web=info");
+    std::env::set_var("RUST_LOG", "actix_web=debug");
     env_logger::init();
 
     HttpServer::new(|| {
+        // let root_dir = );
         let tera = tera::compile_templates!(concat!(env!("CARGO_MANIFEST_DIR"), "/src/templates/**/*"));
 
         App::new()
             .data(tera)
             .wrap(middleware::Logger::default())
             .route("/", web::get().to(index))
+            .route("/s/{filename:.*}", web::get().to(resource))
     })
         .bind("127.0.0.1:8080")
         .unwrap()
