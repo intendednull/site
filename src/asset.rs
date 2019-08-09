bac7a49469543942c085b8301169d0f11d22b0f4
Noah <noah@coronasoftware.net>
2019-08-08 21:56:24 -0700

    Document and refactor

--- src/asset.rs
+++ src/asset.rs
@@ -1,9 +1,9 @@
 use actix_files as fs;
+use serde::Deserialize;
+use image::GenericImageView;
 use std::path::{Path, PathBuf};
 use actix_web::{Result, web};
 
-use serde::Deserialize;
-use image::GenericImageView;
 use super::{util::File};
 
 
@@ -28,21 +28,28 @@ fn asset((file, size): (web::Path<File>, web::Query<FileSize>)) -> Result<fs::Na
     let fpout = match (size.width, size.height) {
         (None, None) => fpin.clone(),
         (w, h) => {
-            let fp = asset_dir.join(
-                format!(".cache/{}-{}x{}", file.title(), w.unwrap_or(0), h.unwrap_or(0))
-            ).with_extension(file.path.extension().unwrap());
+            // Generate a new file name in cache dir based on requested dimensions.
+            // The actual image dimensions are unknown until after it's resized,
+            // which means identical images may exist if the requested dimensions
+            // are different.
+            let fp = format!(".cache/{}-{}x{}", file.title(), w.unwrap_or(0), h.unwrap_or(0));
+            let fp = asset_dir.join(fp)
+                .with_extension(file.path.extension().unwrap());
+            // Verify cache directory exists
             fp.parent().map(|p| std::fs::create_dir(p));
             fp
         }
     };
-    let f = std::fs::File::open(&fpout).or_else(|_| {
-        resize(&fpin, &size, &fpout)
-    })?;
+    // Open file if it exists, otherwise attempt to generate it.
+    let f = std::fs::File::open(&fpout).or_else(
+        |_| resize(&fpin, &size, &fpout))?;
 
     Ok(fs::NamedFile::from_file(f, fpout)?)
 }
 
 
+/// Generate an image that best fits `size` while preserving scale.
+/// Returns generated file.
 fn resize(fp: &PathBuf, size: &FileSize, out: &PathBuf) -> std::io::Result<std::fs::File> {
     let img = image::open(&fp).unwrap();
     let img = img.thumbnail(
@@ -50,21 +57,21 @@ fn resize(fp: &PathBuf, size: &FileSize, out: &PathBuf) -> std::io::Result<std::
         size.height.unwrap_or(img.height())
     );
     let mut file = std::fs::OpenOptions::new()
+        .create_new(true)
         .write(true)
         .read(true)
-        .create(true)
-        .truncate(true)
         .open(&out)?;
-
     let ext = out.extension()
         .and_then(|s| s.to_str())
-        .map_or("".to_string(), |s| s.to_ascii_lowercase());
+        .map_or("".to_owned(), |s| s.to_ascii_lowercase());
 
     img.write_to(&mut file, guess_format(&ext).unwrap()).unwrap();
     Ok(file)
 }
 
 
+/// Attempt to get image format from file extension.
+/// Stolen from https://docs.rs/image/0.22.1/src/image/dynimage.rs.html#755
 fn guess_format(ext: &str) -> std::result::Result<image::ImageFormat, image::ImageError> {
     match ext {
         "jpg" | "jpeg" => Ok(image::ImageFormat::JPEG),
@@ -87,6 +94,7 @@ fn guess_format(ext: &str) -> std::result::Result<image::ImageFormat, image::Ima
 }
 
 
+// TODO remove oldest read cache files when it hits overflow
 fn clean_cache() -> std::io::Result<()> {
     Ok(())
 }
